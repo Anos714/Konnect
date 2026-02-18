8d79af2aed1b380c9aedf6fe3e8e842720a5b27f
Anos714 <sainrahul374@gmail.com>
2026-02-18 12:18:45 +0530

    feat:installed required dependencies(tailwind, daisy ui, tanstack query, react-hot-toast, rect router) and apply basic routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,28 @@
+import { Route, Routes } from "react-router";
+import { Toaster } from "react-hot-toast";
+import Home from "./pages/Home";
+import OnBoarding from "./pages/OnBoarding";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Notification from "./pages/Notification";
+import Chat from "./pages/Chat";
+import CallPage from "./pages/CallPage";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/onboarding" element={<OnBoarding />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/notification" element={<Notification />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="/call" element={<CallPage />} />
+      </Routes>
+      <Toaster />
+    </div>
+  );
 };
 
 export default App;
