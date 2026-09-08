e93976c85b42208a55e85852a0dd07a6079f6c92
Rahul Sain <sainrahul374@gmail.com>
2026-09-08 17:19:20 +0530

    chore: also added teh fallback origin cors url

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin:process.env.HOST_URL,
+    origin:process.env.HOST_URL || "http://localhost:5173",
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     credentials: true,
   }),
